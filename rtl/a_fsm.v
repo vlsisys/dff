3fc334cd7b8e6dd6a2de5d09915436865a133015
vlsisys <woongchoi@sookmyung.ac.kr>
2025-07-10 20:06:05 +0900

    comment

--- rtl/a_fsm.v
+++ rtl/a_fsm.v
@@ -65,15 +65,40 @@ module a_fsm
 		end
 	end
 
-	reg		[5:0]	cnt_ref;
+	reg		[3:0]	cnt_addr;
+	always @(posedge i_clk or negedge i_rstn) begin
+		if(!i_rstn) begin
+			cnt_addr <= 0;
+		end else begin
+			case(c_state)
+				S_DONE	: cnt_addr <= 0;
+				S_IDLE	: cnt_addr <= cnt_addr + 1;
+			endcase
+		end
+	end
+
+	reg		[3:0]	cnt_ref;
 	always @(posedge i_clk or negedge i_rstn) begin
 		if(!i_rstn) begin
 			cnt_ref <= 0;
 		end else begin
 			case(c_exec)
 				E_INIT   : cnt_ref <= 0;
-				E_UPDATE : cnt_ref	<= cnt_ref + 1;
-				E_DONE   : cnt_ref	<= cnt_ref + 1;
+				E_UPDATE : cnt_ref <= cnt_ref + 1;
+				E_DONE   : cnt_ref <= cnt_ref + 1;
+			endcase
+		end
+	end
+
+	reg		[3:0]	cnt_dat;
+	always @(posedge i_clk or negedge i_rstn) begin
+		if(!i_rstn) begin
+			cnt_dat <= 0;
+		end else begin
+			case(c_exec)
+				E_INIT   : cnt_dat <= 0;
+				E_UPDATE : cnt_dat <= cnt_dat + 1;
+				E_DONE   : cnt_dat <= cnt_dat + 1;
 			endcase
 		end
 	end
@@ -81,30 +106,36 @@ module a_fsm
 	// Next State Logic
 	always @(*) begin
 		case(c_state)
-			S_IDLE      : n_state = i_start            ? S_FIX_TCLK  : S_IDLE;
-			S_FIX_TCLK  : n_state = (n_exec == E_DONE) ? S_MIN_TQ    : S_FIX_TCLK;
-			S_MIN_TQ    : n_state = (n_exec == E_DONE) ? S_MIN_TD    : S_MIN_TQ;
-			S_MIN_TD    : n_state = (n_exec == E_DONE) ? S_TD_SAMPLE : S_MIN_TD;
-			S_TD_SAMPLE : n_state = (n_exec == E_DONE) ? S_TQ_SAMPLE : S_TD_SAMPLE;
-			S_TQ_SAMPLE : n_state = (n_exec == E_DONE) ? S_DONE      : S_TQ_SAMPLE;
+			S_IDLE      : n_state = i_start            ? S_FIX_TCLK  : c_state;
+			S_FIX_TCLK  : n_state = (n_exec == E_DONE) ? S_MIN_TQ    : c_state;
+			S_MIN_TQ    : n_state = (n_exec == E_DONE) ? S_MIN_TD    : c_state;
+			S_MIN_TD    : n_state = (n_exec == E_DONE) ? S_TD_SAMPLE : c_state;
+			S_TD_SAMPLE : n_state = (n_exec == E_DONE) ? S_TQ_SAMPLE : c_state;
+			S_TQ_SAMPLE : n_state = (n_exec != E_DONE) ? c_state     :
+				                    (cnt_addr == 15)   ? S_DONE      : S_FIX_TCLK;
 			S_DONE      : n_state = S_IDLE;
 		endcase
 	end
 
 	always @(*) begin
 		case(c_exec)
-			E_INIT   : n_exec = (p_state != c_state) ? E_UPDATE : E_INIT;
-			E_UPDATE : n_exec = (cnt_exec == 15)     ? E_DONE   : E_UPDATE;
+			E_INIT   : n_exec = (p_state != c_state) ? E_UPDATE : c_exec;
+			E_UPDATE : n_exec = (cnt_exec == 15)     ? E_DONE   : c_exec;
 			E_DONE   : n_exec = E_INIT;
+			default  : n_exec = c_exec;
 		endcase
 	end
-
-	
 
 	// Output Logic
 	always @(*) begin
 		case(c_state)
 			S_FIX_TCLK  : begin
+				case(c_exec)
+					E_INIT   : o_ref_dly_sel	= 2**9-1;
+					E_UPDATE : o_ref_dly_sel	= i_mux_out ? 2**9-1-;
+					E_DONE   : o_ref_dly_sel	= 2**9-1;
+					default  : o_ref_dly_sel	= 2**9-1;
+				endcase
 			end
 			S_MIN_TQ    : 
 			S_MIN_TD    : 
