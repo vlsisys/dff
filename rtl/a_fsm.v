43a8e25631c924e006719eeab2e0d9e53874cc3f
vlsisys <woongchoi@sookmyung.ac.kr>
2025-07-19 21:19:35 +0900

    comment

--- rtl/a_fsm.v
+++ rtl/a_fsm.v
@@ -7,43 +7,47 @@
 
 module a_fsm
 (	
-	output reg			o_mode,
-	output reg	[2:0]	o_dut_addr,
-	output reg	[9:0]	o_ref_dly_sel,
-	output reg	[8:0]	o_dat_dly_sel,
-	output reg	[8:0]	o_clk_dly_sel,
-	output reg	[1:0]	o_mux_sel,
+	output reg	[3:0]	o_dut_addr,
+	output reg	[9:0]	o_ref_dly,
+	output reg	[8:0]	o_dat_dly,
+	output reg			o_edge_sel,
+	output reg			o_sigs_sel,
+	output reg			o_check,
 	input		[1:0]	i_mode,
-	input				i_mux_out,
 	input				i_start,
+	input				i_cff_out,
 	input				i_clk,
 	input				i_rstn
 );
 
 	
 // --------------------------------------------------
-	localparam	M_SE_R		= 2'd0;
-	localparam	M_SE_F		= 2'd1;
+// Mode
+	localparam	REF_DLY_BIT	= 10;
+	localparam	DAT_DLY_BIT	= 9;
+
+	localparam	NUM_SAMPLE	= 4;
+	localparam	NUM_DUT		= 40;
+
+	localparam	M_SU_R		= 2'd0;
+	localparam	M_SU_F		= 2'd1;
 	localparam	M_HD_R		= 2'd2;
 	localparam	M_HD_F		= 2'd3;
 
-	localparam	S_IDLE		= 3'd0;
-	localparam	S_FIX_TCLK	= 3'd1;
-	localparam	S_MIN_TQ	= 3'd2;
-	localparam	S_MIN_TD	= 3'd3;
-	localparam	S_TQ_SAMPLE	= 3'd4;
-	localparam	S_TD_SAMPLE	= 3'd5;
-	localparam	S_DONE		= 3'd6;
+	localparam	S_IDLE		= 4'd 0;
+	localparam	S_FIX_TCLK	= 4'd 1;
+	localparam	S_MIN_TQ_D	= 4'd 2;
+	localparam	S_MIN_TQ_Q	= 4'd 3;
+	localparam	S_MIN_TD_D0	= 4'd 4;
+	localparam	S_MIN_TD_D1	= 4'd 5;
+	localparam	S_MIN_TD_Q	= 4'd 6;
+	localparam	S_SAMPLE_D	= 4'd 7;
+	localparam	S_SAMPLE_Q	= 4'd 8;
+	localparam	S_DONE		= 4'd15;
 
-	localparam	E_INIT		= 2'd0;
-	localparam	E_UPDATE	= 2'd1;
-	localparam	E_DONE		= 2'd2;
-
-	reg			[2:0]	p_state ;
-	reg			[2:0]	c_state ;
-	reg			[2:0]	n_state ;
-	reg			[2:0]	c_exec  ;
-	reg			[2:0]	n_exec  ;
+	reg			[3:0]	p_state ;
+	reg			[3:0]	c_state ;
+	reg			[3:0]	n_state ;
 
 	// State Register
 	always @(posedge i_clk or negedge i_rstn) begin
@@ -56,12 +60,31 @@ module a_fsm
 		end
 	end
 
-	// State Register
+	reg		[3:0]	cnt_state;
 	always @(posedge i_clk or negedge i_rstn) begin
 		if(!i_rstn) begin
-			c_exec	<= E_INIT;
+			cnt_state	<= 0;
 		end else begin
-			c_exec	<= n_exec;
+			if (c_state == n_state) begin
+				cnt_state	<= cnt_state + 1;
+			end else begin
+				cnt_state	<= 0;
+			end
+		end
+	end
+
+	reg		[2:0]	cnt_smpls;
+	always @(posedge i_clk or negedge i_rstn) begin
+		if(!i_rstn) begin
+			cnt_smpls	<= 0;
+		end else begin
+			if (c_state == S_SAMPLE_Q && n_state == S_SAMPLE_D) begin
+				cnt_smpls	<= cnt_smpls + 1;
+			else if (c_state == S_DONE) begin
+				cnt_smpls	<= 0;
+			end else begin
+				cnt_smpls	<= cnt_smpls;
+			end
 		end
 	end
 
@@ -71,80 +94,44 @@ module a_fsm
 			cnt_addr <= 0;
 		end else begin
 			case(c_state)
-				S_DONE	: cnt_addr <= 0;
-				S_IDLE	: cnt_addr <= cnt_addr + 1;
+				S_DONE	: cnt_addr <= cnt_addr + 1;
+				S_IDLE	: cnt_addr <= 0;
 			endcase
 		end
-	end
-
-	reg		[3:0]	cnt_ref;
-	always @(posedge i_clk or negedge i_rstn) begin
-		if(!i_rstn) begin
-			cnt_ref <= 0;
-		end else begin
-			case(c_exec)
-				E_INIT   : cnt_ref <= 0;
-				E_UPDATE : cnt_ref <= cnt_ref + 1;
-				E_DONE   : cnt_ref <= cnt_ref + 1;
-			endcase
-		end
-	end
-
-	reg		[3:0]	cnt_dat;
-	always @(posedge i_clk or negedge i_rstn) begin
-		if(!i_rstn) begin
-			cnt_dat <= 0;
-		end else begin
-			case(c_exec)
-				E_INIT   : cnt_dat <= 0;
-				E_UPDATE : cnt_dat <= cnt_dat + 1;
-				E_DONE   : cnt_dat <= cnt_dat + 1;
-			endcase
-		end
-	end
-
-	// Next State Logic
-	always @(*) begin
-		case(c_state)
-			S_IDLE      : n_state = i_start            ? S_FIX_TCLK  : c_state;
-			S_FIX_TCLK  : n_state = (n_exec == E_DONE) ? S_MIN_TQ    : c_state;
-			S_MIN_TQ    : n_state = (n_exec == E_DONE) ? S_MIN_TD    : c_state;
-			S_MIN_TD    : n_state = (n_exec == E_DONE) ? S_TD_SAMPLE : c_state;
-			S_TD_SAMPLE : n_state = (n_exec == E_DONE) ? S_TQ_SAMPLE : c_state;
-			S_TQ_SAMPLE : n_state = (n_exec != E_DONE) ? c_state     :
-				                    (cnt_addr == 15)   ? S_DONE      : S_FIX_TCLK;
-			S_DONE      : n_state = S_IDLE;
-		endcase
 	end
 
 	always @(*) begin
-		case(c_exec)
-			E_INIT   : n_exec = (p_state != c_state) ? E_UPDATE : c_exec;
-			E_UPDATE : n_exec = (cnt_exec == 15)     ? E_DONE   : c_exec;
-			E_DONE   : n_exec = E_INIT;
-			default  : n_exec = c_exec;
+		case(c_state)
+			S_IDLE      : n_state = i_start                        ? c_state : S_FIX_TCLK ;
+			S_FIX_TCLK	: n_state = (cnt_state != REF_DLY_BIT + 1) ? c_state : S_MIN_TQ_D ;
+			S_MIN_TQ_D	: n_state = (cnt_state != REF_DLY_BIT + 1) ? c_state : S_MIN_TQ_Q ;
+			S_MIN_TQ_Q	: n_state = (cnt_state != REF_DLY_BIT + 1) ? c_state : S_MIN_TD_D0;
+			S_MIN_TD_D0 : n_state = (cnt_state != DAT_DLY_BIT + 1) ? c_state : S_MIN_TD_D1;
+			S_MIN_TD_D1 : n_state = (cnt_state != REF_DLY_BIT + 1) ? c_state : S_MIN_TD_Q ;
+			S_MIN_TD_Q  : n_state = (cnt_state != REF_DLY_BIT + 1) ? c_state : S_SAMPLE_D ;
+			S_SAMPLE_D  : n_state = (cnt_state != DAT_DLY_BIT + 1) ? c_state : S_SAMPLE_Q ;
+			S_SAMPLE_Q  : n_state = (cnt_state != REF_DLY_BIT + 1) ? c_state :
+				                    (cnt_smpls == NUM_SAMPLE  - 1) ? S_DONE  : S_SAMPLE_D ;
+			S_DONE		: n_state = (cnt_addr  == NUM_DUT     - 1) ? S_IDLE  : S_FIX_TCLK ;
 		endcase
 	end
 
 	// Output Logic
-	always @(*) begin
-		case(c_state)
-			S_FIX_TCLK  : begin
-				case(c_exec)
-					E_INIT   : o_ref_dly_sel	= 2**9-1;
-					E_UPDATE : o_ref_dly_sel	= i_mux_out ? 2**9-1-;
-					E_DONE   : o_ref_dly_sel	= 2**9-1;
-					default  : o_ref_dly_sel	= 2**9-1;
-				endcase
+	always @(posedge i_clk or negedge i_rstn) begin
+		if (!i_rstn) begin
+			o_ref_dly	<= 2**REF_DLY_BIT-1;
+		end else begin
+			if (c_state != S_IDLE || c_state != S_MIN_TD_D0 || c_state != S_DONE) begin
+				if (cnt_state == REF_DLY_BIT + 1) begin
+					o_ref_dly	<= 2**REF_DLY_BIT-1;
+				end else begin
+					o_ref_dly	<= i_cff_out ? o_ref_dly 
+				end
+			end else begin
+				o_ref_dly	<= 2**REF_DLY_BIT-1;
 			end
-			S_MIN_TQ    : 
-			S_MIN_TD    : 
-			S_TD_SAMPLE : 
-			S_TQ_SAMPLE : 
-			S_DONE      : 
-		endcase
+		end
 	end
-
 
 
 endmodule
